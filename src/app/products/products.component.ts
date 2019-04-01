6ff16e5d37af066ac3fe6f3136afbeb6c3113719
rimon19 <mominur.hossain@outlook.com>
2019-04-01 13:02:34 +0600

    menubarIssueResolve

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -50,32 +50,6 @@ export class ProductsComponent implements OnInit  {
 
   async ngOnInit() {
 
-    $.getScript('assets/js/jquery-3.2.1.min.js');
-    $.getScript('assets/js/popper.min.js');
-    $.getScript('assets/js/bootstrap.min.js');
-    $.getScript('assets/vendors/revolution/js/jquery.themepunch.tools.min.js');
-    $.getScript('assets/vendors/revolution/js/jquery.themepunch.revolution.min.js');
-    $.getScript('assets/vendors/revolution/js/extensions/revolution.extension.actions.min.js');
-    $.getScript('assets/vendors/revolution/js/extensions/revolution.extension.video.min.js');
-    $.getScript('assets/vendors/revolution/js/extensions/revolution.extension.slideanims.min.js');
-    $.getScript('assets/vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js');
-    $.getScript('assets/vendors/revolution/js/extensions/revolution.extension.navigation.min.js');
-    $.getScript('assets/vendors/revolution/js/extensions/revolution.extension.slideanims.min.js');
-    $.getScript('assets/vendors/counterup/jquery.waypoints.min.js');
-    $.getScript('assets/vendors/counterup/jquery.counterup.min.js');
-    $.getScript('assets/vendors/owl-carousel/owl.carousel.min.js');
-    $.getScript('assets/vendors/bootstrap-selector/js/bootstrap-select.min.js');
-    $.getScript('assets/vendors/image-dropdown/jquery.dd.min.js');
-    $.getScript('assets/js/smoothscroll.js');
-    $.getScript('assets/vendors/isotope/imagesloaded.pkgd.min.js');
-    $.getScript('assets/vendors/isotope/isotope.pkgd.min.js');
-    $.getScript('assets/vendors/magnify-popup/jquery.magnific-popup.min.js');
-    $.getScript('assets/vendors/vertical-slider/js/jQuery.verticalCarousel.js');
-    $.getScript('assets/vendors/jquery-ui/jquery-ui.js');
-    $.getScript('assets/js/theme.js');
-   
-    
-
     this.cart$ = await this.shoppingCartService.getCart();
     this.populateProducts();
      
